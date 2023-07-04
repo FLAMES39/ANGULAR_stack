f9b0cd1c0ce5b94baba3b60ee26f214ef11f4be9
FLAMES39 <114187269+FLAMES39@users.noreply.github.com>
2023-07-04 12:35:58 +0300

    updates

--- src/app/add-student/add-student.component.ts
+++ src/app/add-student/add-student.component.ts
@@ -1,13 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
   styleUrls: ['./add-student.component.css']
 })
-export class AddStudentComponent {
+export class AddStudentComponent implements AfterViewChecked,AfterViewInit{
+  ngAfterViewInit(): void {
+    console.log("WACHA");  }
+  ngAfterViewChecked(): void {
+    console.log("twaaa");
+    
+  }
   @Output() add:EventEmitter<{name:string,age:number}>=new EventEmitter()
+  @ViewChild('name') inputName!:ElementRef
   addUser(name:string,age:string){
   this.add.emit({name,age:+age})
+  console.log(this.inputName.nativeElement.value);
   }
+
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.css']
 
 })
-export class HomeComponent {
+export class HomeComponent implements OnChanges, OnInit,DoCheck,AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked,OnDestroy{
   // name:string='CHRISTIAN ABIODUN'
   // send(event:Event){
   //   let value= (event.target as HTMLInputElement).value
@@ -26,19 +26,78 @@ export class HomeComponent {
    
   // }
  
-users:{
-  name:string,
-  age:number}[]=[]
-  getCurrentDateTime(): Date {
-    const currentDateTime: Date = new Date();
-    return currentDateTime;
+// users:{
+//   name:string,
+//   age:number}[]=[]
+//   getCurrentDateTime(): Date {
+//     const currentDateTime: Date = new Date();
+//     return currentDateTime;
+//   }
+//   day= new Date().getDay()
+//   addUser(name:string,age:string){
+//     this.users.push({name,age:+age})
+//     return this.day;
+//   }
+//   deleteUser(i:number){
+//   this.users.splice(i,1)
+//   }
+
+
+
+constructor(){
+  console.log('constructor logged');
+  
+}
+
+
+
+
+
+@Input({required:true}) name=""
+@ViewChild('title') title!:ElementRef
+@ViewChild('lorem') lorem!:ElementRef
+ngOnChanges(changes:SimpleChanges){
+  console.log('on changes called');
+  console.log(changes);
+  
+}
+ngOnInit(){
+    console.log('on init called ');
+ 
+    
+    
+}
+ngDoCheck(): void {
+  console.log('ng dochek is running');
+  
+ }
+ ngAfterContentInit(): void {
+  console.log('aftercontentinit executed');
+  
+
+  
+}
+ ngAfterContentChecked(): void {
+   console.log('aftercontentchecked executed');
+   
+ }
+
+ ngAfterViewInit(): void {
+  console.log("View init");
+  
+  if(this.title){
+  console.log(this.title.nativeElement.textContext);
+
   }
-  day= new Date().getDay()
-  addUser(name:string,age:string){
-    this.users.push({name,age:+age})
-    return this.day;
-  }
-  deleteUser(i:number){
-  this.users.splice(i,1)
-  }
+  
+}
+ngAfterViewChecked(): void {
+  console.log("View checked");
+
+}
+ngOnDestroy(): void {
+  console.log(' Destroyed');
+  
+}
+
 }
